03dbed22f87a1b759553e75462f30bd4b251f363
Anna Ikoki <ikokianna@gmail.com>
2021-09-15 14:20:51 +0200

    Install web server for chrome extension to load local files

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
 "use strict";
-function hello() {
-    console.log('Hello World!!');
-    fetch('https://quizapi.io/api/v1/questions -G \
--d apiKey=YOUR_API_KEY \')
-  .then(response => response.json())
-  .then(data => console.log(data));
+
+// get DOM Elements
+const question = document.getElementById('question');
+const btn = document.getElementById('btn');
+
+const questions = 'questions.json';
+
+async function getQuestion() {    
+//   console.log('Hello World!!');
+//   fetch questions
+// error chrome: https://stackoverflow.com/questions/49971575/chrome-fetch-api-cannot-load-file-how-to-workaround
+  const response = await fetch(questions);
+//   get response
+// first parse the JSON so you have an actual object (JSON.parse())
+  const data = await response.json();
+// display data
+  console.log(data);
+
+//   change btn text to submit question. Avoid .innerText as it skiks <>
+btn.textContent = 'Submit Answer';
+// btn.innerHTML = 'Submit Answer';
+// btn.innerHTML = 'Submit Answer';
 }
-hello();
 
-(()=>{
-fetch('http://example.com/movies.json')
-  .then(response => response.json())
-  .then(data => console.log(data));
-})
+// getQuestion();
 
+// Listen to events
+btn.addEventListener('click', getQuestion);
 
-
-// TODO: Move page to display different questions
+// TODO: change btn text from Get Question to Submit Answer
+// TODO: Display different questions
 // TODO: Verify questions
 // TODO: Give feedback to user
 // TODO: Add hint
 // TODO: Create different types of answers
+// TODO: Store values in the database - skip for now
