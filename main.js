b3b6e9f178c4ead489ea01e78195121ec28925c9
Anna Ikoki <ikokianna@gmail.com>
2021-09-15 11:07:34 +0200

    Create quiz data in a json file

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 "use strict";
 function hello() {
     console.log('Hello World!!');
+    fetch('https://quizapi.io/api/v1/questions -G \
+-d apiKey=YOUR_API_KEY \')
+  .then(response => response.json())
+  .then(data => console.log(data));
 }
 hello();
+
+(()=>{
+fetch('http://example.com/movies.json')
+  .then(response => response.json())
+  .then(data => console.log(data));
+})
+
+
+
+// TODO: Move page to display different questions
+// TODO: Verify questions
+// TODO: Give feedback to user
+// TODO: Add hint
+// TODO: Create different types of answers
